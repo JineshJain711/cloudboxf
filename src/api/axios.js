851d9ed3c82cf04d5ac52fd5ec288a0eca518c5f
Jinesh jain <jineshjain711@gmail.com>
2026-02-12 21:14:32 +0530

    Update API base URL in axios configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cloud-box-kohl.vercel.app/api/v1',
+  baseURL: 'https://cloud-box-seven.vercel.app/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
